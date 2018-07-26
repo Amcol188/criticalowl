9a2b5d3859b1eb905c00f9f205f7ed445851ec04
Juan Villela <juan@villela.co>
2018-07-25 22:31:29 -0400

    Initial site structure
    Created Hugo site along with it’s directory and config.
    Added packe.json for used packages.
    Added gulpfile.js for critical css insertion.
    Added netlify config.
    Added placeholder icons and favicons.
    Added Lato fonts.
    Added font optimization scripts.
    Added CSS reset and inital base styling.
    Added single, summary, list, list item, and baseof templates.
    Added image and link shortcodes.
    Added header, footer, social, pagination, and meta partials.
    Added rss and robots files.
    Updated gitignore.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp         = require('gulp'),
+    rename       = require('gulp-rename'),
+    concat       = require('gulp-concat-util'),
+    postcss      = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssnano      = require('cssnano'),
+    sass         = require('gulp-sass');
+
+// Critical CSS
+gulp.task('critical', function() {
+  var plugins = [
+    autoprefixer({browsers: ['last 2 version']}),
+    cssnano()
+  ];
+  return gulp.src('assets/scss/critical.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss(plugins))
+  // wrap with style tags
+  .pipe(concat.header('<style>'))
+  .pipe(concat.footer('</style>'))
+  // convert it to an include file
+  .pipe(rename({
+      basename: 'critical',
+      extname: '.html'
+    }))
+  // insert file
+  .pipe(gulp.dest('layouts/partials'));
+});
+
+// Watch asset folder for changes
+gulp.task('watch', ['critical'], function () {
+  gulp.watch('assets/scss/critical.scss', ['critical'])
+});
+
+// Run Watch as default
+gulp.task('default', ['watch']);
+
+// Build
+gulp.task('build', ['critical']);
