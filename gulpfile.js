3200ce7b165e41c8c15ce36c5a89993389aaf5bf
Juan C Villela <juan@villela.co>
2018-08-16 22:56:31 -0400

    Image optimization updates
    Gulp now properly converts, resizes and minifies images.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,6 @@
-var gulp						 = require('gulp'),
-    rename					 = require('gulp-rename'),
-    concat					 = require('gulp-concat-util'),
-    postcss					 = require('gulp-postcss'),
-    autoprefixer		   = require('autoprefixer'),
-    cssnano					 = require('cssnano'),
-    sass				     = require('gulp-sass'),
-    cache            = require('gulp-cache'),
-		gm               = require('gulp-gm'),
-    imagemin				 = require('gulp-imagemin'),
-		imageminPngquant = require('imagemin-pngquant'),
-		imageminMozjpeg	 = require('imagemin-mozjpeg');
-
-// Critical CSS
-gulp.task('critical', function() {
+var gulp = require('gulp'),  rename = require('gulp-rename'),  concat = require('gulp-concat-util'),  postcss = require('gulp-postcss'),  autoprefixer = require('autoprefixer'),  cssnano = require('cssnano'),  sass = require('gulp-sass'),  cache = require('gulp-cache'),  imagemin = require('gulp-imagemin'),  imageminPngquant = require('imagemin-pngquant'),  imageminMozjpeg = require('imagemin-mozjpeg'),
+  gm = require('gulp-gm'),
+  imageResize = require('gulp-image-resize');// Critical CSSgulp.task('critical', function() {
   var plugins = [
     autoprefixer({browsers: ['last 2 version']}),
     cssnano()
@@ -32,46 +20,18 @@ gulp.task('critical', function() {
   .pipe(gulp.dest('layouts/partials'));
 });
 
-// Image Convertion
-gulp.task('convert', function () {
-  gulp.src('src/img/*.png')
-    .pipe(gm(function (gmfile) {
-      return gmfile.setFormat('jpg');
-    }))
-    .pipe(gulp.dest('src/img'));
-})
+// Image Conversiongulp.task('convert', function() {  return gulp.src('assets/img/*.png')
+  .pipe(gm(function (gmfile) {
+    return gmfile.setFormat('jpg');
+  }))  .pipe(gulp.dest('assets/img'));});
 
-// Image Optimization
-gulp.task('imgOptim', function() {
-  return gulp.src(['src/img/*.{png,jpg}'])
-    .pipe(cache(imagemin([
-      imageminPngquant({
-        speed: 1,
-        quality: 98 //lossy settings
-      }),
-      imagemin.jpegtran({
-        progressive: true
-      }),
-      imageminMozjpeg({
-        quality: 90
-      })
-    ])))
-    .pipe(gulp.dest('src/img'));
-});
-// Watch asset folder for changes
-gulp.task('watch', ['critical', 'imgOptim', 'convert'], function () {
-  gulp.watch('assets/scss/fonts.scss', ['critical'])
-  gulp.watch('assets/scss/variables.scss', ['critical'])
-  gulp.watch('assets/scss/extends.scss', ['critical'])
-  gulp.watch('assets/scss/reset.scss', ['critical'])
-  gulp.watch('assets/scss/layout.scss', ['critical'])
-  gulp.watch('assets/scss/critical.scss', ['critical'])
-  gulp.watch('assets/img/*', ['convert'])
-  gulp.watch('assets/img/*', ['imgOptim'])
-});
-
-// Run Watch as default
-gulp.task('default', ['watch']);
-
-// Build
-gulp.task('build', ['critical', 'imgOptim', 'convert']);
+// Image Conversiongulp.task('resize', function() {  return gulp.src('assets/img/*.jpg')
+  .pipe(imageResize({
+    width : 700
+  }))
+  .pipe(gulp.dest('assets/img'));});// Image Optimizationgulp.task('imgOptim', function() {  return gulp.src('assets/img/*.jpg')  .pipe(cache(imagemin([    imageminMozjpeg({      quality: 90,
+      progressive: true    })  ])))  .pipe(gulp.dest('assets/img'));});
+// Watch asset folder for changesgulp.task('watch', ['critical', 'convert', 'resize', 'imgOptim'], function() {  gulp.watch('assets/scss/fonts.scss', ['critical']);  gulp.watch('assets/scss/variables.scss', ['critical']);  gulp.watch('assets/scss/extends.scss', ['critical']);  gulp.watch('assets/scss/reset.scss', ['critical']);  gulp.watch('assets/scss/layout.scss', ['critical']);  gulp.watch('assets/scss/critical.scss', ['critical']);
+  gulp.watch('assets/img/*', ['convert']);
+  gulp.watch('assets/img/*', ['resize']);  gulp.watch('assets/img/*', ['imgOptim']);
+});// Run Watch as defaultgulp.task('default', ['watch']);// Buildgulp.task('build', ['critical', 'convert', 'resize', 'imgOptim']);
