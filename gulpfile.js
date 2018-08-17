b7c4c450d5c6a36dfa0d2917da67fef1fa8bf4dd
Juan C Villela <juan@villela.co>
2018-08-16 23:08:40 -0400

    Further image optimizations
    Polish Gulp task for better performance.
    Further optimized images.

--- gulpfile.js
+++ gulpfile.js
@@ -24,14 +24,11 @@ var gulp = require('gulp'),  rename = require('gulp-rename'),  concat = requir
   .pipe(gm(function (gmfile) {
     return gmfile.setFormat('jpg');
   }))  .pipe(gulp.dest('assets/img'));});
-
-// Image Conversiongulp.task('resize', function() {  return gulp.src('assets/img/*.jpg')
+// Image Optimizationgulp.task('imgOptim', function() {  return gulp.src('assets/img/*.jpg')
   .pipe(imageResize({
     width : 700
-  }))
-  .pipe(gulp.dest('assets/img'));});// Image Optimizationgulp.task('imgOptim', function() {  return gulp.src('assets/img/*.jpg')  .pipe(cache(imagemin([    imageminMozjpeg({      quality: 90,
+  }))  .pipe(cache(imagemin([    imageminMozjpeg({      quality: 90,
       progressive: true    })  ])))  .pipe(gulp.dest('assets/img'));});
-// Watch asset folder for changesgulp.task('watch', ['critical', 'convert', 'resize', 'imgOptim'], function() {  gulp.watch('assets/scss/fonts.scss', ['critical']);  gulp.watch('assets/scss/variables.scss', ['critical']);  gulp.watch('assets/scss/extends.scss', ['critical']);  gulp.watch('assets/scss/reset.scss', ['critical']);  gulp.watch('assets/scss/layout.scss', ['critical']);  gulp.watch('assets/scss/critical.scss', ['critical']);
-  gulp.watch('assets/img/*', ['convert']);
-  gulp.watch('assets/img/*', ['resize']);  gulp.watch('assets/img/*', ['imgOptim']);
-});// Run Watch as defaultgulp.task('default', ['watch']);// Buildgulp.task('build', ['critical', 'convert', 'resize', 'imgOptim']);
+// Watch asset folder for changesgulp.task('watch', ['critical', 'convert', 'imgOptim'], function() {  gulp.watch('assets/scss/fonts.scss', ['critical']);  gulp.watch('assets/scss/variables.scss', ['critical']);  gulp.watch('assets/scss/extends.scss', ['critical']);  gulp.watch('assets/scss/reset.scss', ['critical']);  gulp.watch('assets/scss/layout.scss', ['critical']);  gulp.watch('assets/scss/critical.scss', ['critical']);
+  gulp.watch('assets/img/*', ['convert']);  gulp.watch('assets/img/*', ['imgOptim']);
+});// Run Watch as defaultgulp.task('default', ['watch']);// Buildgulp.task('build', ['critical', 'convert', 'imgOptim']);
